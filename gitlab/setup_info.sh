c21774a686861214fc7598d2703e7968305fff97
Leif Ringstad <leif@symbolia.com>
2019-01-27 19:51:04 +0100

    Update setup info for kubernetes setup in gitlab

--- gitlab/setup_info.sh
+++ gitlab/setup_info.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-echo Server: $(kubectl cluster-info | grep 'Kubernetes master' | awk '/http/ {print $NF}')
+IP=$(minikube ip)
+echo "\nInformation for gitlab setup"
+echo "API  ip: Server: $(kubectl cluster-info | grep 'Kubernetes master' | awk '/http/ {print $NF}')"
+echo "    url: HTTPS:  https://kubeapi.$IP.nip.io:8443"
+echo "         HTTP:   http://kubeapi.$IP.nip.io:8443"
 echo "\nToken:"
 echo "$(kubectl get secret $(kubectl get secrets | grep gitlab-token | cut -f1 -d ' ') -o jsonpath="{['data']['token']}" | base64 --decode)"
 echo "\nCA:"
 echo "$(kubectl get secret $(kubectl get secrets | grep gitlab-token | cut -f1 -d ' ') -o jsonpath="{['data']['ca\.crt']}" | base64 --decode)"
+
