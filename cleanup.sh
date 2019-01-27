cf83a80ab545893758d70e9cfe4ba32bd45e25ed
Leif Ringstad <leif@symbolia.com>
2019-01-27 20:49:07 +0100

    Clean ssh known hosts

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+ssh-keygen -R $(minikube ip)
+ssh-keygen -R gitlab.$(minikube ip).nip.io
+ssh-keygen -R $(minikube ip).nip.io
 minikube delete
 echo "Cleaning up virtualbox host only dhcp ips - NOTE: Any host-only ip already set will be deleted on ALL hostonly interfaces"
 rm ~/.config/VirtualBox/HostInterfaceNetworking-vboxnet*
