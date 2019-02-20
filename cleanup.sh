3253648c7e9d445d0c5aa4aaebde751e7624d03f
Leif Ringstad <leif@symbolia.com>
2019-02-20 09:22:21 +0100

    Cleanup vbox0

--- cleanup.sh
+++ cleanup.sh
@@ -10,4 +10,4 @@ ssh-keygen -R "[$IP]:2222"
 minikube delete
 # Cleanup virtual ips
 echo "Cleaning up virtualbox host only dhcp ips - NOTE: Any host-only ip already set will be deleted on ALL hostonly interfaces"
-rm ~/.config/VirtualBox/HostInterfaceNetworking-vboxnet*
+rm ~/.config/VirtualBox/HostInterfaceNetworking-vboxnet0*
